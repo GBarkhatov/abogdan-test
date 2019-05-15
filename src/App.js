67dc0ee5f6fbe9bf192ddf8b95e460210b7f88e7
Govard Barkhatov <gbarkhatov@gmail.com>
2019-05-15 18:34:33 +1000

    add basic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Usual from './Usual'
 
-// people: https://swapi.co/api/people/
-// planets: https://swapi.co/api/planets/
-
 const App = props => (
   <div>
     <Usual />
--- src/Usual.js
+++ src/Usual.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 
 class Usual extends Component {
+  state = {
+    fetchData: null,
+    axiosData: null
+  }
+
+  componentDidMount () {
+    const peopleURL = 'https://swapi.co/api/people/'
+    const planetsURL = 'https://swapi.co/api/planets/'
+
+    fetch(peopleURL)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => console.error(error))
+  }
+
   render () {
-    return (
-      <div>
-        Inside Usual component
-      </div>
-    )
+    return <div>Inside Usual component</div>
   }
 }
 
