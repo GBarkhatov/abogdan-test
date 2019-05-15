032ec5d4efe7aca8f810704ec463356fc6fbde6d
Govard Barkhatov <gbarkhatov@gmail.com>
2019-05-15 18:30:08 +1000

    add usual component

---
+++ src/Usual.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Usual extends Component {
+  render () {
+    return (
+      <div>
+        Inside Usual component
+      </div>
+    )
+  }
+}
+
+export default Usual
