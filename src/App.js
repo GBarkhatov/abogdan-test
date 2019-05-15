c5f7621a454f0a15bd968291194b8ba3e9bff011
Govard Barkhatov <gbarkhatov@gmail.com>
2019-05-15 18:23:41 +1000

    convert to class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = props => (
-  <div>
-    Inside App
-  </div>
-)
+class App extends Component {
+  render () {
+    return (
+      <div>
+        Inside App
+      </div>
+    )
+  }
+}
 
 export default App
